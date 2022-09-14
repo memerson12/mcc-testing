1d032c0f0eddccc1488ce928625962b4cf507ed3
Michael Emerson <emerson.michael12@gmail.com>
2022-09-13 20:47:08 -0400

    Vercel handler

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ const firebaseConfig = {
     measurementId: "G-FYG6WJKFPT"
 }
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export default async function handler(request, response) {
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
 
-const eventInfo = (await getDoc(doc(db, "config", "event-info"))).data();
-const allTeamNames = JSON.parse((await getDoc(doc(db, "config", "static"))).data()['language']);
-const teamNames = allTeamNames[eventInfo['teamNameKey']];
-const data = {
-    event: `mcc${eventInfo['episode']}`,
-    teams: {},
-    twitchUsers: [],
-    youtubeUsers: {}
-};
-const creators = await getDocs(collection(db, "creators"));
-creators.forEach((creatorDoc) => {
-    const creator = creatorDoc.data()
-    if (creator['participating']) {
-        const username = creator['username']
-        const teamName = teamNames[creator['team']];
-        data['teams'][teamName] ? data['teams'][teamName].push(username) : data['teams'][teamName] = [username]
-        if(creator['platform'] === 'youtube') {
-            data['youtubeUsers'][username] = [creator['id'], 0];
-        } else {
-            data['twitchUsers'].push(username);
+    const eventInfo = (await getDoc(doc(db, "config", "event-info"))).data();
+    const allTeamNames = JSON.parse((await getDoc(doc(db, "config", "static"))).data()['language']);
+    const teamNames = allTeamNames[eventInfo['teamNameKey']];
+    const data = {
+        event: `mcc${eventInfo['episode']}`,
+        teams: {},
+        twitchUsers: [],
+        youtubeUsers: {}
+    };
+    const creators = await getDocs(collection(db, "creators"));
+    creators.forEach((creatorDoc) => {
+        const creator = creatorDoc.data()
+        if (creator['participating']) {
+            const username = creator['username']
+            const teamName = teamNames[creator['team']];
+            data['teams'][teamName] ? data['teams'][teamName].push(username) : data['teams'][teamName] = [username]
+            if(creator['platform'] === 'youtube') {
+                data['youtubeUsers'][username] = [creator['id'], 0];
+            } else {
+                data['twitchUsers'].push(username);
+            }
         }
-    }
-})
-
-return data
-
+    })
+    response.status(200).send(data);
+}
 // main-1b25edc7.9952c7b5.js
